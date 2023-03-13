1f0edb91a1d11a652c6d87e6c84f4d938e79f6b5
Mike <nicholasfierke97@gmail.com>
2023-03-13 12:57:36 -0400

    Home

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Header from "./components/Header";
+import { Card, Col, Row } from "react-materialize";
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <h1>Welcome to Full Plate</h1>
-      <p>Here you can find and save your favorite recipes.</p>
+      <Row>
+        <Col m={6}>
+          <Card
+            className="blue-grey darken-1"
+            textClassName="white-text"
+            title="Welcome to Full Plate"
+          >
+            <p>Here you can find and save your favorite recipes.</p>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
