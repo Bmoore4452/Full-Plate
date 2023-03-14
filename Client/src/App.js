a9e3ada2286772a1f15c1c8af2737452ac2f7f4e
unknown <joseph.harley02@gmail.com>
2023-03-14 11:59:13 -0400

    displays recipes on default url

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import { UserProvider } from './utils/UserContext';
+// import { UserProvider } from './utils/UserContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './pages/Home';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -13,18 +14,18 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <UserProvider>
-        <Router>
+      <Router>
         <Header />
         <div className="container">
           <Routes>
-          
+            <Route 
+              path='/'
+              element={<Home />}
+            />
           </Routes>
         </div>
-        </Router>
         <Footer />
-      </UserProvider>
-      
+      </Router>
     </ApolloProvider>
   );
 }
--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -1,22 +1,41 @@
 import React from "react";
-import Header from "./components/Header";
-import { Card, Col, Row } from "react-materialize";
+import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_RECIPES } from "../utils/queries";
 
 const Home = () => {
+  const { loading, data } = useQuery(QUERY_RECIPES);
+
+  const recipeList = data?.recipes || [];
+
   return (
     <div>
-      <Header />
-      <Row>
-        <Col m={6}>
-          <Card
-            className="blue-grey darken-1"
-            textClassName="white-text"
-            title="Welcome to Full Plate"
-          >
-            <p>Here you can find and save your favorite recipes.</p>
-          </Card>
-        </Col>
-      </Row>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {recipeList.map((recipe) => {
+            return (
+              <div key={recipe.title} className="row">
+                <div className="col s12 m7">
+                  <div className="card">
+                    <div className="card-image">
+                      <img src={recipe.image} alt="food"/>
+                      <span className="card-title">{recipe.title}</span>
+                    </div>
+                    <div className="card-content">
+                      <p>{recipe.description}</p>
+                    </div>
+                    <div className="card-action">
+                      <Link>View Full Recipe</Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
