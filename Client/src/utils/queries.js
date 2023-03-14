40e58bc526b14fed21983858c816b42d2366b509
unknown <joseph.harley02@gmail.com>
2023-03-14 14:04:12 -0400

    displays single recipe when you click the link

--- Client/src/utils/queries.js
+++ Client/src/utils/queries.js
@@ -1,8 +1,21 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_RECIPES = gql`
-    query recipes {
+    query getRecipes {
         recipes {
+            _id
+            description
+            image
+            steps
+            title
+        }
+    }
+`;
+
+export const QUERY_SINGLE_RECIPE = gql`
+    query getRecipe($recipeId: ID!) {
+        recipe(recipeId: $recipeId) {
+            _id
             description
             image
             steps
